37f42caa6ff629b1e3693699ddacb0668d31351c
petang <siskape4@gmail.com>
2023-02-27 12:43:22 +0100

    delete unused .sql

--- src/main/resources/candidate.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO candidate (name, surname, email, note) VALUES
-  ('Jan', 'Janek', 'jan.janek@gmail.com', 'Senior Java dev, znalost Python, C++, SQL, Docker'),  
-  ('Karel', 'Karlin', 'karelkarlin@seznam.cz', 'Jsem Karel ahoj'),
-  ('Petr', 'Petrov', 'pp@gmail.cz', 'Jsem Petr'),
-  ('Lukas', 'Kasel', 'lukekasel@gmail.com', 'Jsem Luke zdravim'),
-  ('Oto', 'Toto', 'tenota@gmail.com', 'Jsem Oto'),
-  ('Stepan', 'Vtipny', 'step@gmail.com', 'Jsem Stepa');
